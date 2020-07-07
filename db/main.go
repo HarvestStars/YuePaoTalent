c97421d9e2a4e583143c9057441bc531b36fbee6
dell <574817827@qq.com>
2020-07-07 18:40:13 +0800

    add curd

--- db/main.go
+++ db/main.go
@@ -12,6 +12,23 @@ type Product struct {
 }
 
 func main() {
-	db, _ := gorm.Open("mysql", "user:password@/dbname?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", "user:password@/dbname?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic("failed to connect database")
+	}
 	defer db.Close()
+
+	// 创建
+	db.Create(&Product{Code: "L1212", Price: 1000})
+
+	// 读取
+	var product Product
+	db.First(&product, 1)                   // 查询id为1的product
+	db.First(&product, "code = ?", "L1212") // 查询code为l1212的product
+
+	// 更新 - 更新product的price为2000
+	db.Model(&product).Update("Price", 2000)
+
+	// 删除 - 删除product
+	db.Delete(&product)
 }
